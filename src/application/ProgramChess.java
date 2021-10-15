ce4883706d96c986f45023573238ba658b491aae
Rita Santos <ritabr@gmail.com>
2021-10-15 18:28:12 -0300

    Movendo as peças

--- src/application/ProgramChess.java
+++ src/application/ProgramChess.java
@@ -1,15 +1,57 @@
 package application;
 
+import java.util.Scanner;
+
 import boardgame.Board;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class ProgramChess {
 
 	public static void main(String[] args) {
 		
-		ChessMatch chessmatch = new ChessMatch();
-		UI.printBoard(chessmatch.getPieces());
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		while (true) {
+		UI.printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.print("Source: ");
+		ChessPosition source = UI.readChessPosition(sc);
+		
+		System.out.println();
+		System.out.print("Target: ");
+		ChessPosition target = UI.readChessPosition(sc);
+		
+		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		
+		}
+	
+	}
+	
+}
+/*
+public static void main(String[] args) {
+
+Scanner sc = new Scanner(System.in);
+
+ChessMatch chessMatch = new ChessMatch();
+List<ChessPiece> captured = new ArrayList<>();
+
+while (!chessMatch.getCheckMate()) {
+	try {
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
+		System.out.println();
+		System.out.print("Source: ");
+		ChessPosition souce = UI.readChessPosition(sc);
+		
+		boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+		UI.clearScreen();
 		
 	}
-
+	
 }
+
+}*/
