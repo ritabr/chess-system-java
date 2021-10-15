20a0b8684c3b12566ab4c6ab64991485f7c23294
Rita Santos <ritabr@gmail.com>
2021-10-15 15:45:37 -0300

    Tabuleiro de xadrex e impressão do tabuleiro

--- src/application/ProgramChess.java
+++ src/application/ProgramChess.java
@@ -1,12 +1,14 @@
 package application;
 
 import boardgame.Board;
+import chess.ChessMatch;
 
 public class ProgramChess {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board(8, 8);
+		ChessMatch chessmatch = new ChessMatch();
+		UI.printBoard(chessmatch.getPieces());
 		
 	}
 
