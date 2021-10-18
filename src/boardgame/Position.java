d8ff4a2eab83dba72e7ba9c1926d6d528deb8a2d
Rita Santos <ritabr@gmail.com>
2021-10-18 10:56:13 -0300

    Implementando movimentos possíveis da Torre

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -30,6 +30,11 @@ public class Position {
 	public Position() {
 		super();
 	}
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
 	@Override
 	public String toString() {
 		return row + ", " + column;
